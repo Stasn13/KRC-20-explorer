78f3104d5daa06675b65a4f1342e289733fc0ee3
Stas <stasn139@gmail.com>
2025-03-20 18:24:27 +0000

    utils and constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,2 @@
+export const baseUrl="https://api.kasplex.org/v1"
+export const logoUrl="https://krc20-assets.kas.fyi/icons"
---
+++ src/lib/utils.ts
@@ -0,0 +1,25 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function currentDate() {
+  return new Date().toISOString().split("T")[0]
+}
+
+export function formatNumber(value: string | number) {
+  const amount = Number(value)
+  if (amount >= 1_000_000_000) {
+    return (amount / 1_000_000_000).toFixed(2) + 'B'; 
+  } else if (amount >= 1_000_000) {
+    return (amount / 1_000_000).toFixed(2) + 'M';
+  } else if (amount >= 1_000) {
+    return (amount / 1_000).toFixed(2) + 'K';
+  } else {
+    return amount.toFixed(2).toString(); // Less than 1K
+  }
+}
+
+export const shortenAddress = (addr: string) => addr?.split(":")[1].slice(0, 6);
